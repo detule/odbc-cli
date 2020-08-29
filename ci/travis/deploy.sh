bdccaf4ccfe4d37341b1599a6fe5f53d0bbb7812
detule <ogjoneski@gmail.com>
2020-08-29 23:41:15 +0000

    ci: deployment hopefully

---
+++ ci/travis/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -ue
+
+if ! [[ "$TRAVIS_TAG" == "" ]]
+then
+  echo "Deploying wheel to test pypi"
+  twine upload --repository-url https://test.pypi.org/legacy/ -u $TEST_PYPI_USERNAME -p $TEST_PYPI_PASSWORD  $TRAVIS_BUILD_DIR/dist/odbcli*.tar.gz
+  echo "Deploying wheel to pypi"
+  twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD  $TRAVIS_BUILD_DIR/build/src/cython/dist/Cyanodbc*.whl
+else
+  echo "Deploying wheel to test pypi"
+  twine upload --repository-url https://test.pypi.org/legacy/ -u $TEST_PYPI_USERNAME -p $TEST_PYPI_PASSWORD  $TRAVIS_BUILD_DIR/dist/odbcli*.tar.gz
+fi
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requirements = [
 
 setuptools.setup(
     name = "odbcli", # Replace with your own username
-    version = "0.0.1.13",
+    version = "0.0.2",
     author = "Oliver Gjoneski",
     author_email = "ogjoneski@gmail.com",
     description = "ODBC Client",
