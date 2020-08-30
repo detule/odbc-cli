63745cadae08977b41ea6b22ee6f68f1a05800aa
detule <ogjoneski@gmail.com>
2020-08-30 01:07:28 +0000

    ci: Fixup travis deployment script

--- ci/travis/deploy.sh
+++ ci/travis/deploy.sh
@@ -5,7 +5,7 @@ then
   echo "Deploying wheel to test pypi"
   twine upload --repository-url https://test.pypi.org/legacy/ -u $TEST_PYPI_USERNAME -p $TEST_PYPI_PASSWORD  $TRAVIS_BUILD_DIR/dist/odbcli*.tar.gz
   echo "Deploying wheel to pypi"
-  twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD  $TRAVIS_BUILD_DIR/build/src/cython/dist/Cyanodbc*.whl
+  twine upload -u $PYPI_USERNAME -p $PYPI_PASSWORD  $TRAVIS_BUILD_DIR/dist/odbcli*.tar.gz
 else
   echo "Deploying wheel to test pypi"
   twine upload --repository-url https://test.pypi.org/legacy/ -u $TEST_PYPI_USERNAME -p $TEST_PYPI_PASSWORD  $TRAVIS_BUILD_DIR/dist/odbcli*.tar.gz
