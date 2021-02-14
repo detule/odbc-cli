7d6cf067a27248c1b6fc6f8fddaa18b7b56bdb1b
detule <ogjoneski@gmail.com>
2021-02-14 19:51:08 +0000

    dbmetadata: Now case insensitive
    Rather than nested dict, example {"table": {"AdventureWorks2017":
    {"Person": {"Address": {}}, ...}, ...}, ...} to represent
    
    AdventureWorks2017.Person.Address
    
    Now: {"table": {"adventureworks2017": ("AdventureWorks2017": {"person":
    ("Person", {...}), ... )}.
    
    That is to say, whereas previously we had [cased]: {}, each entry is now
    [lowercase]: ([cased], {...}).

--- odbcli/dbmetadata.py
+++ odbcli/dbmetadata.py
@@ -10,65 +10,81 @@ class DbMetadata():
         with self._lock:
             for metadata in self._dbmetadata.values():
                 for catalog in names:
-                    metadata[catalog] = {}
+                    metadata[catalog.lower()] = (catalog, {})
         return
 
-    def get_catalogs(self, obj_type: str = "table") -> list:
+    def get_catalogs(self, obj_type: str = "table", cased: bool = True) -> list:
         """ Retrieve catalogs as the keys for _dbmetadata[obj_type]
             If no keys are found it returns None.
         """
         with self._lock:
-            res = self._dbmetadata[obj_type].keys()
+            if cased:
+                res = [casedkey for casedkey, mappedvalue in self._dbmetadata[obj_type].values()]
+            else:
+                res = list(self._dbmetadata[obj_type].keys())
+
 
         if len(res) == 0:
             return None
 
-        return list(res)
+        return res
 
     def extend_schemas(self, catalog, names: list) -> None:
         """ This method will force/create [catalog] dictionary
             in the event that len(names) > 0, and overwrite if
             anything was there to begin with.
         """
+        catlower = catalog.lower()
+        cat_cased = catalog
         if len(names):
             with self._lock:
                 for metadata in self._dbmetadata.values():
-                    metadata[catalog] = {}
+                    # Preserve casing if an entry already there
+                    if catlower in metadata.keys() and len(metadata[catlower]):
+                        cat_cased = metadata[catlower][0]
+                    metadata[catlower] = (cat_cased, {})
                     for schema in names:
-                        metadata[catalog][schema] = {}
+                        metadata[catlower][1][schema.lower()] = (schema, {})
         return
 
-    def get_schemas(self, catalog: str, obj_type: str = "table") -> list:
+    def get_schemas(self, catalog: str, obj_type: str = "table", cased: bool = True) -> list:
         """ Retrieve schemas as the keys for _dbmetadata[obj_type][catalog]
             If catalog is not part of the _dbmetadata[obj_type] keys will return
             None.
         """
 
-        cats = self.get_catalogs(obj_type = obj_type)
-        if cats is None or catalog not in cats:
+        catlower = catalog.lower()
+        cats = self.get_catalogs(obj_type = obj_type, cased = False)
+        if cats is None or catlower not in cats:
             return None
 
         with self._lock:
-            res = self._dbmetadata[obj_type][catalog].keys()
+            if cased:
+                res = [casedkey for casedkey, mappedvalue in self._dbmetadata[obj_type][catlower][1].values()]
+            else:
+                res = list(self._dbmetadata[obj_type][catlower][1].keys())
 
-        return list(res)
+
+        return res
 
     def extend_objects(self, catalog, schema, names: list, obj_type: str) -> None:
+        catlower = catalog.lower()
+        schlower = schema.lower()
         if len(names):
             with self._lock:
-                for metadata in self._dbmetadata.values():
+                for otype in self._dbmetadata.keys():
                     # Loop over tables, views, functions
-                    if catalog not in metadata.keys():
-                        metadata[catalog] = {}
-                    if schema not in metadata[catalog].keys():
-                        metadata[catalog][schema] = {}
+                    if catlower not in self._dbmetadata[otype].keys():
+                        self._dbmetadata[otype][catlower] = (catalog, {})
+                    if schlower not in self._dbmetadata[otype][catlower][1].keys():
+                        self._dbmetadata[otype][catlower][1][schlower] = (schema, {})
                 for obj in names:
-                    self._dbmetadata[obj_type][catalog][schema][obj] = {}
+                    self._dbmetadata[obj_type][catlower][1][schlower][1][obj.lower()] = (obj, {})
         # If we passed nothing then take out that element entirely out
         # of the dict
         else:
             with self._lock:
-                del self._dbmetadata[obj_type][catalog][schema]
+                del self._dbmetadata[obj_type][catlower][1][schlower]
 
         return
 
@@ -78,12 +94,14 @@ class DbMetadata():
             not one of the keys in _dbmetadata[obj_type][catalog] will return None
         """
 
-        schemas = self.get_schemas(catalog = catalog, obj_type = obj_type)
-        if schemas is None or schema not in schemas:
+        catlower = catalog.lower()
+        schlower = schema.lower()
+        schemas = self.get_schemas(catalog = catalog, obj_type = obj_type, cased = False)
+        if schemas is None or schlower not in schemas:
             return None
 
         with self._lock:
-            res = self._dbmetadata[obj_type][catalog][schema].keys()
+            res = [casedkey for casedkey, mappedvalue in self._dbmetadata[obj_type][catlower][1][schlower][1].values()]
 
         return list(res)
 
